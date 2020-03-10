3cdfb045fe5a45afbf4f2013db040505abf6cfa9
santhosh1994m <45985363+santhosh1994m@users.noreply.github.com>
2020-03-10 19:50:31 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,8 @@
-provider "docker" {
+provider "aws" {
  
 }
 
-# Create a container
-resource "docker_container" "foo" {
-  image = "${docker_image.ubuntu.latest}"
-  name  = "foo"
-}
 
-resource "docker_image" "ubuntu" {
-  name = "ubuntu:latest"
+output "add"{
+    value   = "${1+1}"
 }
